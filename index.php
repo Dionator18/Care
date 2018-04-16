2748d3d93f647a0053c25e042dee9f1b9d1c5347
dionsantegoets <dionsantegoets1999@gmail.com>
2018-04-16 15:20:44 +0200

    personen.php

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <title>Care | Home</title>
             <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" type="text/css" media="screen" href="../css/cssreset.css"/>
             <link rel="stylesheet" type="text/css" media="screen" href="css/stylesheet.css" />
             <script src="js/index.js"></script>
     </head>
     <body>
-        
+
     </body>
 </html>
--- pages/personen.php
+++ pages/personen.php
@@ -5,11 +5,23 @@
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <title>Care | Personen</title>
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link rel="stylesheet" type="text/css" media="screen" href="../css/personen.css"/>
             <link rel="stylesheet" type="text/css" media="screen" href="../css/cssreset.css"/>
+            <link rel="stylesheet" type="text/css" media="screen" href="../css/personen.css"/>
+            <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
             <script src="js/index.js"></script>
     </head>
     <body>
-        
+        <div id="title_div">
+            <h1>Wie bent u?</h1>
+        </div>    
+        <div id="personen">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div id="link_div">
+            <p><a href="#">+ Persoon Toevoegen</a></p>
+        </div>    
     </body>
 </html>
